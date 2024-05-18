b3bb75d2fd649ed51d4cffa3e7fbaaf1c5867683
Khaledshatah12 <khaledshatah37@outlook.sa>
2024-05-18 11:05:51 +0300

    Create Khaled12q.js

---
+++ plugins/Khaled12q.js
@@ -0,0 +1,98 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    conn.relayMessage(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: 'ستو💙ــرـــ💙ـيـات🙈💙'
+            },
+            body: {
+              text: 'K H A L E D S H A T A H 🖤.'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: 'الاوامر 🖤',
+                    sections: [
+                      {
+                        title: 'Khaled_Shatah🖤.',
+                        highlight_label: '🖤',
+                        rows: [
+                          {
+                            header: 'القائمة',
+                            title: 'الاوامر',
+                            description: '',
+                            id: '.menu'
+                          },
+                          {
+                            header: 'الذكاء الصناعي',
+                            title: ' مثال .ai ما هي عاصمه سوريا',
+                            description: '',
+                            id: '.ai'
+                          },
+                          {
+                            header: 'تاغ جماعي',
+                            title: '🖤',
+                            description: '',
+                            id: '.hidetag'
+                          },
+                          {
+                            header: 'تحميل التطبيقات',
+                            title: ' مثال.apk whatsapp',
+                            description: '',
+                            id: '.apk'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: ''
+                },
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: 'معلومات المطور 🖤',
+                    sections: [
+                      {
+                        title: 'Khaled_shatah🖤.',
+                        highlight_label: '🖤',
+                        rows: [
+                          {
+                            header: '.owner',
+                            title: 'https://wa.me/963980677944',
+                            description: '',
+                            id: '.owner'
+                          },
+                          {
+                            header: 'قناة المطور',
+                            title: '.channel',
+                            description: '',
+                            id: '.channel'
+                          },
+                          {
+                            header: 'تحميل اغاني',
+                            title: ' مثال .song اسم الاغنيه',
+                            description: '',
+                            id: '.song'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: ''
+                }
+              ]
+            }
+          }
+        }
+      }
+    }, {})
+}
+
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['الاوامر']
+
+export default handler
